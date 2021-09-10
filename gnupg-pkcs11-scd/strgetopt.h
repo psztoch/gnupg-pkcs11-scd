ecd549bc7617a3349c0c3c11e59007ef2242e82e
Alon Bar-Lev <alon.barlev@gmail.com>
2021-09-10 22:55:47 +0300

    gnupg-pkcs11-scd: use strgetarg for all line arguments
    gnupg-2.3 seems to add command arguments for all commands.

---
+++ gnupg-pkcs11-scd/strgetopt.h
@@ -0,0 +1,25 @@
+#ifndef __STRGETOPT_H
+#define __STRGETOPT_H
+
+#define strgtopt_no_argument 0
+#define strgtopt_required_argument 1
+
+struct strgetopt_option {
+	const char *name;
+	int has_arg;
+	char **value;
+	int *found;
+};
+
+const char *
+strgetopt_getopt(
+	const char * const str,
+	const struct strgetopt_option * const options
+);
+
+void
+strgetopt_free(
+	const struct strgetopt_option * const options
+);
+
+#endif
